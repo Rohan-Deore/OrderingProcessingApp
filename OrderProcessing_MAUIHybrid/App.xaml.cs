b30dedc4ad4b487ad8afa07d9ec413e9afb6f9aa
Rohan-Deore <deorerohan.2612@gmail.com>
2024-08-29 15:20:15 +0530

    Projects for all levels and UI added

---
+++ OrderProcessing_MAUIHybrid/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace OrderProcessing_MAUIHybrid
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ OrderProcessing_MAUIHybrid/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace OrderProcessing_MAUIHybrid
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
