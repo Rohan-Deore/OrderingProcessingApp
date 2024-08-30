77c47d8205af4b70ed07989510df0a45a8ee5859
Rohan-Deore <deorerohan.2612@gmail.com>
2024-08-30 12:47:52 +0530

    Moved everything to make it work but failed

---
+++ VIewModel/MainViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewModel
+{
+    public class MainViewModel : ViewModelBase
+    {
+        IUiService UiService;
+
+        private string someValue;
+
+        public string SomeValue
+        {
+            get
+            {
+                return someValue;
+            }
+            set
+            {
+                if (value != someValue)
+                {
+                    someValue = value;
+                }
+
+                OnPropertyChanged("SomeValue");
+            }
+        }
+    }
+}
