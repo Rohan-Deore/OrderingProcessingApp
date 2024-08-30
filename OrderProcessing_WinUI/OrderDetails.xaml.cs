7229f43b2240bbea9ed98b1810203fab1d2404f8
Rohan-Deore <deorerohan.2612@gmail.com>
2024-08-30 18:49:18 +0530

    Order details and control created

---
+++ OrderProcessing_WinUI/OrderDetails.xaml.cs
@@ -0,0 +1,70 @@
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace OrderProcessing_WinUI
+{
+    public class Author
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public DateTime DOB { get; set; }
+        public string BookTitle { get; set; }
+        public bool IsMVP { get; set; }
+    }
+
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class OrderDetails : Page
+    {
+        public OrderDetails()
+        {
+            this.InitializeComponent();
+
+            List<Author> authors = new List<Author>();
+            authors.Add(new Author()
+            {
+                ID = 101,
+                Name = "Mahesh Chand",
+                BookTitle = "Graphics Programming with GDI+",
+                DOB = new DateTime(1975, 2, 23),
+                IsMVP = false
+            });
+
+            authors.Add(new Author()
+            {
+                ID = 201,
+                Name = "Mike Gold",
+                BookTitle = "Programming C#",
+                DOB = new DateTime(1982, 4, 12),
+                IsMVP = true
+            });
+
+            authors.Add(new Author()
+            {
+                ID = 244,
+                Name = "Mathew Cochran",
+                BookTitle = "LINQ in Vista",
+                DOB = new DateTime(1985, 9, 11),
+                IsMVP = true
+            });
+
+            //OrderGrid.item
+        }
+    }
+}
