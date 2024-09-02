3bfd9b923fd438a7d94111c9b7ee2d4cdaf7b106
Rohan-Deore <deorerohan.2612@gmail.com>
2024-09-02 16:55:41 +0530

    Order details view added

--- VIewModel/OrderDetailsVM.cs
+++ VIewModel/OrderDetailsVM.cs
@@ -1,7 +1,71 @@
-﻿namespace ViewModel
+﻿using System.Collections.ObjectModel;
+
+namespace ViewModel
 {
-    public class OrderDetailsVM
+    public class Order : ViewModelBase
+    {
+        private string partName = string.Empty;
+        public string PartName 
+        {
+            get
+            { 
+                return partName;
+            }
+            set
+            {
+                if (partName != value)
+                {
+                    partName = value;
+                    OnPropertyChanged(nameof(PartName));
+                }
+            }
+        }
+
+        private int partID;
+
+        public int PartID 
+        {
+            get
+            { 
+                return partID;
+            }
+
+            set
+            {
+                if (partID != value)
+                {
+                    partID = value;
+                    OnPropertyChanged(nameof(PartID));
+                }
+            }
+        }
+
+    }
+
+    public class OrderDetailsVM : ViewModelBase
     {
         public string TabName { get; private set; } = "Order details";
+
+        private ObservableCollection<Order> orders = new ObservableCollection<Order>();
+        public ObservableCollection<Order> Orders
+        {
+            get
+            {
+                return orders;
+            }
+            set
+            {
+                orders = value;
+                OnPropertyChanged(nameof(Orders));
+            }
+        }
+
+        public OrderDetailsVM() 
+        {
+            orders.Add(new Order { PartID = 1, PartName = "Machine 1" });
+            orders.Add(new Order { PartID = 2, PartName = "Machine 2" });
+            orders.Add(new Order { PartID = 3, PartName = "Machine 3" });
+            orders.Add(new Order { PartID = 4, PartName = "Machine 4" });
+        }
     }
 }
