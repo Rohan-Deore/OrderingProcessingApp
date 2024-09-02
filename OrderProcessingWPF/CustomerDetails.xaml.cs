77d77f0d04746813e102a8801aab014055c31f73
Rohan-Deore <deorerohan.2612@gmail.com>
2024-09-02 13:24:59 +0530

    Customer details view model binding completed

---
+++ OrderProcessingWPF/CustomerDetails.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Controls;
+using ViewModel;
+
+namespace OrderProcessingWPF
+{
+    /// <summary>
+    /// Interaction logic for CustomerDetails.xaml
+    /// </summary>
+    public partial class CustomerDetails : UserControl
+    {
+        public CustomerDetails()
+        {
+            InitializeComponent();
+            DataContext = new CustomerDetailsVM();
+        }
+    }
+}
--- VIewModel/CustomerDetailsVM.cs
+++ VIewModel/CustomerDetailsVM.cs
@@ -3,6 +3,77 @@
     public class CustomerDetailsVM : ViewModelBase
     {
         public string TabName { get; private set; } = "Customer details";
+        
+        private string customerName = "Enter customer name";
 
+        public string CustomerName 
+        {
+            get
+            {
+                return customerName;
+            }
+            set
+            {
+                if (customerName != value)
+                {
+                    customerName = value;
+                    OnPropertyChanged(nameof(CustomerName));
+                }
+            }
+        }
+
+        private string customerLocation = string.Empty;
+
+        public string CustomerLocation
+        {
+            get
+            {
+                return customerLocation;
+            }
+            set
+            {
+                if (customerLocation != value)
+                {
+                    customerLocation = value;
+                    OnPropertyChanged(nameof(CustomerLocation));
+                }
+            }
+        }
+
+        private DateTime orderDate = DateTime.Today;
+
+        public DateTime OrderDate
+        {
+            get 
+            { 
+                return orderDate; 
+            }
+            set 
+            {
+                if (orderDate != value)
+                {
+                    orderDate = value;
+                    OnPropertyChanged(nameof(OrderDate));
+                }
+            }
+        }
+
+        private DateTime deliveryDate = DateTime.Today;
+
+        public DateTime DeliveryDate
+        {
+            get 
+            { 
+                return deliveryDate; 
+            }
+            set 
+            {
+                if (orderDate != value)
+                {
+                    orderDate = value;
+                    OnPropertyChanged(nameof(DeliveryDate));
+                }
+            }
+        }
     }
 }
