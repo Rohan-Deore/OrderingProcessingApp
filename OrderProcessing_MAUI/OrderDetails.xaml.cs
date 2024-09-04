f368c4dce9bdb7c9b42c691397d8f460fcbff014
Rohan-Deore <deorerohan.2612@gmail.com>
2024-09-04 13:19:28 +0530

    Grid dynamic creation  completed

--- OrderProcessing_MAUI/OrderDetails.xaml.cs
+++ OrderProcessing_MAUI/OrderDetails.xaml.cs
@@ -17,6 +17,30 @@ public partial class OrderDetails : ContentPage
 
     private void ContentPage_Loaded(object sender, EventArgs e)
     {
-        
+        if (orderDetailsVM != null && DeviceInfo.Current.Platform == DevicePlatform.Android)
+        {
+            for (int i = 0;  i < orderDetailsVM.Orders.Count; i++)
+            {
+                var order = orderDetailsVM.Orders[i];
+                AddRow(order, i);
+            }
+        }
+    }
+
+    private void AddRow(Order order, int rowCount)
+    {
+        OrdersGrid.AddRowDefinition(new RowDefinition());
+
+        var newEntry = new Entry();
+        newEntry.Text = order.PartID.ToString();
+        Grid.SetColumn(newEntry, 0);
+        Grid.SetRow(newEntry, rowCount + 1);
+        OrdersGrid.Add(newEntry);
+
+        var newEntry1 = new Entry();
+        newEntry1.Text = order.PartName;
+        Grid.SetColumn(newEntry1, 1);
+        Grid.SetRow(newEntry1, rowCount + 1);
+        OrdersGrid.Add(newEntry1);
     }
 }
