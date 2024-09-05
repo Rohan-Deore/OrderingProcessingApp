5adb1dea18292a1ad59e2183d21d0ee50c066097
Rohan-Deore <deorerohan.2612@gmail.com>
2024-09-05 14:12:28 +0530

    Controller added for APIs

---
+++ WebAPI_DB/Controllers/OrderController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI_DB.Controllers
+{
+    public class OrderController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
